5a81553ca5b9b86602573c3d69230815f3d4abb0
luisguiserrano <luisgui.serrano@gmail.com>
2020-09-30 14:13:11 -0400

    Changing output to float

--- f1_score.py
+++ f1_score.py
@@ -48,7 +48,7 @@ def calculate_f1_score_step(labels, predictions):
     lab = read_json(labels)['labels']
     pred = read_json(predictions)['predictions']
 
-    f1_score = [calculate_f1_score(pred, lab)]
+    f1_score = calculate_f1_score(pred, lab)
 
     f1_score_dict = {}
     f1_score_dict['f1_score'] = f1_score
